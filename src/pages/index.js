8f3a1b39dc2f781c66069f803814b739571bf41a
Alejandro Arango <alejandroarang@gmail.com>
2023-03-15 15:14:10 -0500

    fin part 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,33 @@
 // Step 1: Import React
 import * as React from 'react'
+import Layout from '../components/layout'
+
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <main>
-      <h1>Welcome to my Gatsby site! para mi CV en tech</h1>
+    <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
-      <a href='https://www.gatsbyjs.com/'> Gatsby site</a>
-      
+    </Layout>
 
-      <a href='https://pages.github.com/'> github page site</a>
 
-    </main>
+
+
+    // <main>
+    //   <h1>Welcome to my Gatsby site! para mi CV en tech</h1>
+    //   <p>I'm making this by following the Gatsby Tutorial.</p>
+
+    //   <a href='https://www.gatsbyjs.com/'> Gatsby site</a>
+
+
+    //   <a href='https://pages.github.com/'> github page site</a>
+
+    // </main>
   )
 }
 
 // You'll learn about this in the next task, just copy it for now
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>Alejandro Arango</title>
 
 // Step 3: Export your component
 export default IndexPage
